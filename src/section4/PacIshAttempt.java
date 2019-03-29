1b257a2451f17440254b280779e0241cfec0eda4
alvincvms <48074117+alvincvms@users.noreply.github.com>
2019-03-29 14:23:32 -0700

    Revert "Revert "day 5""
    This reverts commit 8b80f38e98c3ed4f786fe246fa1623975becbac5.

--- src/section4/PacIshAttempt.java
+++ src/section4/PacIshAttempt.java
@@ -1,24 +1,49 @@
 package section4;
 import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+import org.jointheleague.graphical.robot.Robot;
 
-import java.io.IOException;
-
-public class PacIshAttempt {
+public class PacIshAttempt implements KeyEventDispatcher{
 	
-	static boolean gameOn = true;
-	
-	public static void startUp() {
-		
+	 boolean gameOn = true;
+	 int pacY;
+	 int pacX;
+	 int coinX;
+	 int coinY;
+	 int score = 0;
+	public  void startUp() {
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
+		draw();
+		pacY = new Random().nextInt(7) + 2;
+		pacX = new Random().nextInt(16) + 2;
+		coinY = new Random().nextInt(7) + 2;
+		coinX = new Random().nextInt(16) + 2;
+		mainRun();
 	}
 	
-	
-	public static void draw() {
+	public void mainRun() {
+		draw();
+		while (gameOn) {
+			logic();
+			draw();
+		}
+	}
+	public  void draw() {
 		for (int y = 1; y<=9; y++) {
 			for (int x = 1; x<=18; x++) {
-				if (x<18 && y == 1) {
+					
+				if(x == pacX && y == pacY) {
+					System.out.print("O");
+				}
+				else if(x == coinX && y == coinY) {
+					System.out.print("C");
+				}
+				else if (x<18 && y == 1) {
 					System.out.print("#");
 				}
-				if (x == 18) {
+				else if (x == 18) {
 					System.out.println("#");
 				}
 				else if (x<18 && y == 9) {
@@ -35,28 +60,56 @@ public class PacIshAttempt {
 				}
 			}
 		}
+		System.out.println("Score; " + score);
 	}
 
-	public static void move() {
-		
+	public  void logic() {
+		if (pacY == coinY && pacX == coinX) {
+			score = score + 100;
+		}
 	}
-	public static void logic() {
-		
+	
+	
+	public  void turnRight() {
+		pacX = pacX + 1;
 	}
-	public static void aiMove() {
-		
+	public  void turnLeft() {
+		pacX = pacX - 1;
 	}
+	public  void goUp() {
+		pacY = pacY - 1;
+	}
+	public  void goDown() {
+		pacY = pacY + 1;
+	}
+	public boolean dispatchKeyEvent(KeyEvent e) {
+		if (e.getID() == KeyEvent.KEY_PRESSED) {
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					turnRight();
+			}
+			else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+				turnLeft();
+			}
+			else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				goDown();
+			}
+			else if (e.getKeyCode() == 38) {
+				goUp();
+			}	
+		}
+		return false;
+	}
+	
 	
 	
 	public static void main(String[] args) { 
-		
-		startUp();
+		new PacIshAttempt().startUp();
+		/*startUp();
 		draw();
 		while (gameOn) {
-			move();
 			logic();
-			aiMove();
-			}
+			draw();
+		}*/
 		
 	}
 }
